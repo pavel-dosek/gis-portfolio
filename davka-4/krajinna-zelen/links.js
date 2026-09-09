ea4eda640689c766e1ac3170ed8da4a475d88a77
pavel-dosek <pavel.dosek@gmail.com>
2026-09-09 18:39:47 +0200

    Add files via upload

---
+++ davka-4/krajinna-zelen/links.js
@@ -0,0 +1,12 @@
+window.__reportLinks = window.__reportLinks || {};
+window.__reportLinks['davka-4/krajinna-zelen'] = {
+  '12': [
+    {x:11.9, y:14.2, w:75.5, h:2.5, url:'https://aopkcr.maps.arcgis.com/apps/webappviewer/index.html?id=ee190990a1be4ac685d5f7c69c637ae4'},
+    {x:49.3, y:18.4, w:38.3, h:1.1, url:'https://ags.cuzk.gov.cz/geoprohlizec/?p=88'},
+    {x:12.0, y:24.8, w:40.5, h:1.1, url:'https://services.cuzk.cz/shp/ku/epsg-5514/'},
+    {x:11.9, y:29.6, w:60.1, h:1.1, url:'http://geoportal.cuzk.cz/WMS_ORTOFOTO_PUB/WMService.aspx'},
+    {x:11.9, y:33.8, w:44.7, h:1.1, url:'https://www.edpp.cz/vrm_charakteristika-zajmoveho-uzemi/'},
+    {x:11.9, y:38.0, w:75.3, h:2.7, url:'https://www.gobec.cz/vermerovice/#13/1843651.14/6447489.55/EwFgpghAHADA3KAZtewCsMULQRi2gIwBM5EBDHBAYyrIgDZ4A5MpoA'},
+    {x:11.9, y:44.4, w:70.3, h:2.3, url:'https://geoportal.spucr.cz/web/cz/pg_bpejmap#pg_bpejMap!n_x=-591828.8214795538&n_y=-1070633.9750956586&n_zoom=7'}
+  ]
+};
---
+++ davka-4/uzemni-planovani/links.js
@@ -0,0 +1,28 @@
+window.__reportLinks = window.__reportLinks || {};
+window.__reportLinks['davka-4/uzemni-planovani'] = {
+  '28': [
+    {x:37.62, y:11.00, w:17.64, h:4.63, url:'https://mestysdavle.cz/'},
+    {x:11.52, y:17.88, w:42.53, h:3.68, url:'https://land.copernicus.eu/en/products/corine-land-cover'},
+    {x:40.32, y:23.17, w:48.17, h:1.61, url:'https://www.kudyznudy.cz/aktivity/zelezny-most-v-davli-kulisa-k-filmu-most-u-remag'},
+    {x:11.52, y:24.78, w:22.31, h:2.56, url:'https://www.kudyznudy.cz/aktivity/zelezny-most-v-davli-kulisa-k-filmu-most-u-remag'},
+    {x:11.52, y:30.07, w:76.96, h:2.25, url:'https://geoportal.cuzk.cz/(S(dmvejp5ue3q5rz3p5wo15v55))/Default.aspx?mode=TextMeta&text=ortofoto_info&side=ortofoto&menu=23'},
+    {x:11.52, y:32.32, w:26.86, h:3.68, url:'https://geoportal.cuzk.cz/(S(dmvejp5ue3q5rz3p5wo15v55))/Default.aspx?mode=TextMeta&text=ortofoto_info&side=ortofoto&menu=23'},
+    {x:11.52, y:38.25, w:76.96, h:2.25, url:'https://mapy.com/cs/letecka?q=davle&source=muni&id=4238&ds=2&x=14.3824104&y=49.8885293&z=13&ovl=8'},
+    {x:11.52, y:40.51, w:11.67, h:3.68, url:'https://mapy.com/cs/letecka?q=davle&source=muni&id=4238&ds=2&x=14.3824104&y=49.8885293&z=13&ovl=8'},
+    {x:11.52, y:46.44, w:53.73, h:3.68, url:'https://cs.wikipedia.org/wiki/Ostrov_svat%C3%A9ho_Kili%C3%A1na'},
+    {x:11.52, y:52.58, w:49.41, h:3.68, url:'https://mapy.com/cs/letecka?x=15.6252330&y=49.8022514&z=8'},
+    {x:11.52, y:58.71, w:49.41, h:3.68, url:'https://mapy.com/cs/letecka?x=15.6252330&y=49.8022514&z=8'},
+    {x:30.62, y:64.85, w:57.87, h:2.25, url:'https://gis.kr-stredocesky.cz/js/reg_up/?extent=-766476.0582%2C-1053358.1567%2C-763403.668%2C-1051969.9804%2C102067'},
+    {x:11.52, y:67.10, w:49.57, h:3.68, url:'https://gis.kr-stredocesky.cz/js/reg_up/?extent=-766476.0582%2C-1053358.1567%2C-763403.668%2C-1051969.9804%2C102067'},
+    {x:51.24, y:70.78, w:37.24, h:2.46, url:'https://mestysdavle.cz/samosprava/uzemni-plan/'},
+    {x:11.52, y:73.24, w:4.79, h:3.88, url:'https://mestysdavle.cz/samosprava/uzemni-plan/'},
+    {x:11.52, y:79.58, w:63.66, h:3.88, url:'https://vdb.czso.cz/vdbvo2/faces/cs/index.jsf?page=statistiky&katalog=31674'},
+    {x:11.52, y:85.72, w:76.96, h:2.25, url:'https://geoportal.cuzk.cz/(S(m4i31gb2qti0v2k1kze5qnks))/Default.aspx?mode=TextMeta&side=mapy_data50&text=dSady_mapyData50&head_tab=sekce-02-gp&menu=2290'},
+    {x:11.52, y:87.97, w:55.30, h:3.68, url:'https://geoportal.cuzk.cz/(S(m4i31gb2qti0v2k1kze5qnks))/Default.aspx?mode=TextMeta&side=mapy_data50&text=dSady_mapyData50&head_tab=sekce-02-gp&menu=2290'}
+  ],
+  '29': [
+    {x:36.49, y:9.35, w:17.44, h:1.77, url:'https://www.pvl.cz/planovani-v-oblasti-vod/plany-dilcich-povodi?utm_source=chatgpt.com'},
+    {x:36.49, y:14.65, w:18.73, h:1.77, url:'https://www.pvl.cz/hydrologicke-informace/dokumentace-a-vyhodnoceni-povodni/zpravy-o-povodni-pvl?utm_source=chatgpt.com'},
+    {x:36.49, y:19.96, w:22.71, h:1.77, url:'https://www.chmi.cz/-/kvalita-ovzdu%C5%A1%C3%AD-v-%C4%8Dr-2025?utm_source=chatgpt.com'}
+  ]
+};
