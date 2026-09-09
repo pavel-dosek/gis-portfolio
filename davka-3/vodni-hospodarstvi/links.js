6d8ed85cc5ff08d6e377bbcbfbfb069d558ea994
pavel-dosek <pavel.dosek@gmail.com>
2026-09-09 20:21:50 +0200

    Add files via upload

--- davka-3/vodni-hospodarstvi/links.js
+++ davka-3/vodni-hospodarstvi/links.js
@@ -1,1 +1,55 @@
-window.__reportLinks=window.__reportLinks||{};window.__reportLinks['davka-3/vodni-hospodarstvi']={"10": [{"x": 11.52, "y": 39.07, "w": 48.64, "h": 2.67, "url": "https://www.portalobce.cz/povodnovy-plan/zdar-nad-sazavou/"}, {"x": 76.46, "y": 41.74, "w": 12.01, "h": 1.72, "url": "https://dpp.kr-vysocina.cz/pub_CZ063/index.html?0-uvod.htm"}, {"x": 11.52, "y": 43.46, "w": 36.77, "h": 2.67, "url": "https://dpp.kr-vysocina.cz/pub_CZ063/index.html?0-uvod.htm"}], "27": [{"x": 30.15, "y": 17.03, "w": 58.32, "h": 1.72, "url": "https://mze.gov.cz/public/portal/mze/voda/osveta-a-publikace/publikace-a-dokumenty/modre-zpravy"}, {"x": 11.52, "y": 18.75, "w": 20.5, "h": 2.67, "url": "https://mze.gov.cz/public/portal/mze/voda/osveta-a-publikace/publikace-a-dokumenty/modre-zpravy"}, {"x": 59.82, "y": 35.68, "w": 20.51, "h": 2.67, "url": "https://www.voda.gov.cz/"}], "28": [{"x": 11.52, "y": 39.12, "w": 35.89, "h": 2.67, "url": "https://www.calla.cz/atlas/detail.php?id=2183"}, {"x": 11.52, "y": 43.52, "w": 51.19, "h": 2.67, "url": "https://land.copernicus.eu/en/products/corine-land-cover/clc2018"}, {"x": 11.52, "y": 47.91, "w": 49.18, "h": 2.67, "url": "https://hydro.chmi.cz/hppsoldv/hpps_prfdyn.php?seq=2505258"}, {"x": 11.52, "y": 52.3, "w": 44.51, "h": 2.67, "url": "https://hydro.chmi.cz/hppsevlist/download?seq=2505258"}, {"x": 11.52, "y": 56.69, "w": 69.73, "h": 2.67, "url": "https://www.chmi.cz/files/portal/docs/hydro/hydrologicke_rocenky/HR_2004_eprilohy.pdf"}, {"x": 74.66, "y": 59.36, "w": 13.81, "h": 1.72, "url": "https://www.kct-tabor.cz/gymta/vodniprehrady/PilskaUZdaruNadSazavou/index.htm"}, {"x": 11.52, "y": 61.09, "w": 51.95, "h": 2.67, "url": "https://www.kct-tabor.cz/gymta/vodniprehrady/PilskaUZdaruNadSazavou/index.htm"}, {"x": 11.52, "y": 65.48, "w": 60.95, "h": 2.67, "url": "https://www.korunavysociny.cz/katalog/1041-pilska-nadrz-rekreacni-areal-pilak"}, {"x": 11.52, "y": 74.26, "w": 72.5, "h": 2.67, "url": "https://mapy.com/cs/zakladni?source=base&id=1904741&x=15.8957844&y=49.5662800&z=16"}, {"x": 11.52, "y": 78.66, "w": 72.5, "h": 2.67, "url": "https://mapy.com/cs/zakladni?source=base&id=2060506&x=15.9311681&y=49.5613177&z=17"}, {"x": 11.52, "y": 83.05, "w": 41.66, "h": 2.67, "url": "https://cerc.povodnoveplany.cz/lang_cs/clanek/7981/"}, {"x": 11.52, "y": 87.44, "w": 38.19, "h": 2.67, "url": "https://www.povis.cz/html/index.html?povis.htm"}], "29": [{"x": 11.52, "y": 10.14, "w": 51.13, "h": 2.67, "url": "https://www.pvl.cz/portal/SaP/cz/pc/Mereni.aspx?id=SAZD&oid=2"}, {"x": 11.52, "y": 14.53, "w": 76.95, "h": 1.72, "url": "https://www.pvl.cz/files/download/vodohospodarske-informace/vodni-dila-a-nadrze/pilska-u-zdaru.pdf"}, {"x": 11.52, "y": 16.25, "w": 7.93, "h": 2.67, "url": "https://www.pvl.cz/files/download/vodohospodarske-informace/vodni-dila-a-nadrze/pilska-u-zdaru.pdf"}, {"x": 60.43, "y": 18.92, "w": 28.04, "h": 1.72, "url": "https://www.vysocina.eu/turisticke-cile/4711-vodni-nadrz-pilska"}, {"x": 11.52, "y": 20.64, "w": 22.1, "h": 2.67, "url": "https://www.vysocina.eu/turisticke-cile/4711-vodni-nadrz-pilska"}, {"x": 11.52, "y": 25.04, "w": 32.24, "h": 2.67, "url": "https://www.dibavod.cz/index.php?id=27"}, {"x": 11.52, "y": 34.6, "w": 61.25, "h": 2.67, "url": "https://cs.wikipedia.org/wiki/Vodn%C3%AD_n%C3%A1dr%C5%BE_Pilsk%C3%A1"}]};
+window.__reportLinks = window.__reportLinks || {};
+window.__reportLinks['davka-3/vodni-hospodarstvi'] = {
+  '02': [
+    {x:11.90, y:10.80, w:76.12, h:1.60, url:'#page=03'},
+    {x:11.90, y:13.12, w:76.12, h:1.60, url:'#page=03'},
+    {x:11.90, y:15.44, w:76.12, h:1.60, url:'#page=04'},
+    {x:11.90, y:17.75, w:76.12, h:1.60, url:'#page=07'},
+    {x:11.90, y:20.07, w:76.12, h:1.60, url:'#page=10'},
+    {x:11.90, y:22.38, w:76.12, h:1.60, url:'#page=11'},
+    {x:11.90, y:24.70, w:76.12, h:1.60, url:'#page=15'},
+    {x:11.90, y:27.02, w:76.12, h:1.60, url:'#page=17'},
+    {x:11.90, y:29.33, w:76.12, h:1.60, url:'#page=18'},
+    {x:11.90, y:31.65, w:76.12, h:1.60, url:'#page=20'},
+    {x:11.90, y:33.97, w:76.12, h:1.60, url:'#page=21'},
+    {x:11.90, y:36.28, w:76.12, h:1.60, url:'#page=22'},
+    {x:11.90, y:38.60, w:76.12, h:1.60, url:'#page=24'},
+    {x:11.90, y:40.91, w:76.12, h:1.60, url:'#page=25'},
+    {x:11.90, y:43.23, w:76.12, h:1.60, url:'#page=26'},
+    {x:11.90, y:45.55, w:76.12, h:1.60, url:'#page=27'},
+    {x:11.90, y:47.86, w:76.12, h:1.60, url:'#page=27'},
+    {x:11.90, y:50.18, w:76.12, h:1.60, url:'#page=27'},
+    {x:11.90, y:52.50, w:76.12, h:1.60, url:'#page=28'},
+    {x:11.90, y:54.81, w:76.12, h:1.60, url:'#page=28'}
+  ],
+  '27': [
+    {x:60.20, y:35.34, w:19.70, h:1.72, url:'https://www.voda.gov.cz/'},
+    {x:11.90, y:18.52, w:10.46, h:1.72, url:'https://mze.gov.cz/public/portal/mze/voda/osveta-a-publikace/publikace-a-dokumenty/modre-zpravy'},
+    {x:21.32, y:16.80, w:66.79, h:1.72, url:'https://mze.gov.cz/public/portal/mze/voda/osveta-a-publikace/publikace-a-dokumenty/modre-zpravy'}
+  ],
+  '28': [
+    {x:11.90, y:87.60, w:40.85, h:1.72, url:'https://cerc.povodnoveplany.cz/lang_cs/clanek/7981/'},
+    {x:11.90, y:83.21, w:71.69, h:1.72, url:'https://mapy.com/cs/zakladni?source=base&id=2060506&x=15.9311681&y=49.5613177&z=17'},
+    {x:11.90, y:78.81, w:48.15, h:1.72, url:'https://mapy.com/cs/zakladni?source=base&id=1904741&x=15.8957844&y=49.5662800&z=16'},
+    {x:64.55, y:77.21, w:23.55, h:1.60, url:'https://mapy.com/cs/zakladni?source=base&id=1904741&x=15.8957844&y=49.5662800&z=16'},
+    {x:11.90, y:70.02, w:60.14, h:1.72, url:'https://www.korunavysociny.cz/katalog/1041-pilska-nadrz-rekreacni-areal-pilak'},
+    {x:11.90, y:65.63, w:51.20, h:1.72, url:'https://www.kct-tabor.cz/gymta/vodniprehrady/PilskaUZdaruNadSazavou/index.htm'},
+    {x:75.10, y:64.03, w:13.00, h:1.60, url:'https://www.kct-tabor.cz/gymta/vodniprehrady/PilskaUZdaruNadSazavou/index.htm'},
+    {x:11.90, y:61.23, w:68.91, h:1.72, url:'https://www.chmi.cz/files/portal/docs/hydro/hydrologicke_rocenky/HR_2004_eprilohy.pdf'},
+    {x:11.90, y:56.84, w:43.70, h:1.72, url:'https://hydro.chmi.cz/hppsevlist/download?seq=2505258'},
+    {x:11.90, y:52.44, w:48.36, h:1.72, url:'https://hydro.chmi.cz/hppsoldv/hpps_prfdyn.php?seq=2505258'},
+    {x:21.08, y:48.05, w:50.39, h:1.72, url:'https://land.copernicus.eu/en/products/corine-land-cover/clc2018'},
+    {x:11.90, y:43.65, w:35.08, h:1.72, url:'https://www.calla.cz/atlas/detail.php?id=2183'}
+  ],
+  '29': [
+    {x:11.90, y:38.88, w:34.18, h:1.72, url:'https://cs.wikipedia.org/wiki/Vodn%C3%AD_n%C3%A1dr%C5%BE_Pilsk%C3%A1'},
+    {x:61.83, y:37.29, w:26.27, h:1.60, url:'https://cs.wikipedia.org/wiki/Vodn%C3%AD_n%C3%A1dr%C5%BE_Pilsk%C3%A1'},
+    {x:11.90, y:29.32, w:31.43, h:1.72, url:'https://www.dibavod.cz/index.php?id=27'},
+    {x:11.90, y:24.92, w:21.36, h:1.72, url:'https://www.vysocina.eu/turisticke-cile/4711-vodni-nadrz-pilska'},
+    {x:60.89, y:23.32, w:27.22, h:1.60, url:'https://www.vysocina.eu/turisticke-cile/4711-vodni-nadrz-pilska'},
+    {x:11.90, y:20.53, w:7.18, h:1.72, url:'https://www.pvl.cz/files/download/vodohospodarske-informace/vodni-dila-a-nadrze/pilska-u-zdaru.pdf'},
+    {x:11.90, y:18.80, w:76.21, h:1.72, url:'https://www.pvl.cz/files/download/vodohospodarske-informace/vodni-dila-a-nadrze/pilska-u-zdaru.pdf'},
+    {x:11.90, y:14.41, w:50.32, h:1.72, url:'https://www.pvl.cz/portal/SaP/cz/pc/Mereni.aspx?id=SAZD&oid=2'},
+    {x:11.90, y:10.01, w:37.38, h:1.72, url:'https://www.povis.cz/html/index.html?povis.htm'}
+  ]
+};
