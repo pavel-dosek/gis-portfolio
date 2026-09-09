9e713cbe61ce65dff98366f68179447e903e439e
pavel-dosek <pavel.dosek@gmail.com>
2026-09-09 20:20:29 +0200

    Add files via upload

--- davka-2/protierozni-ochrana/links.js
+++ davka-2/protierozni-ochrana/links.js
@@ -1,1 +1,8 @@
-window.__reportLinks=window.__reportLinks||{};window.__reportLinks['davka-2/protierozni-ochrana']={"21": [{"x": 12.49, "y": 35.83, "w": 32.63, "h": 3.53, "url": "https://spu.gov.cz/pudni-sluzba/bpej"}, {"x": 46.06, "y": 35.99, "w": 8.44, "h": 3.2, "url": "https://creativecommons.org/licenses/by/4.0/deed.cs"}, {"x": 12.49, "y": 47.92, "w": 42.99, "h": 3.54, "url": "https://mze.gov.cz/public/app/eagriapp/lpisdata/"}]};
+window.__reportLinks = window.__reportLinks || {};
+window.__reportLinks['davka-2/protierozni-ochrana'] = {
+  '21': [
+    {x:13.81, y:40.22, w:37.83, h:3.60, url:'https://spu.gov.cz/pudni-sluzba/bpej'},
+    {x:52.99, y:40.29, w:10.28, h:3.31, url:'https://creativecommons.org/licenses/by/4.0/deed.cs'},
+    {x:13.81, y:56.72, w:50.52, h:3.60, url:'https://mze.gov.cz/public/app/eagriapp/lpisdata/'}
+  ]
+};
