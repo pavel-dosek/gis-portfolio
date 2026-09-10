bfdb319e2baf11a79e71dc35865c01baf3c6f977
pavel-dosek <pavel.dosek@gmail.com>
2026-09-10 19:19:09 +0200

    Add files via upload

--- davka-2/protierozni-ochrana/links.js
+++ davka-2/protierozni-ochrana/links.js
@@ -1,8 +1,15 @@
 window.__reportLinks = window.__reportLinks || {};
 window.__reportLinks['davka-2/protierozni-ochrana'] = {
-  '21': [
-    {x:13.81, y:40.22, w:37.83, h:3.60, url:'https://spu.gov.cz/pudni-sluzba/bpej'},
-    {x:52.99, y:40.29, w:10.28, h:3.31, url:'https://creativecommons.org/licenses/by/4.0/deed.cs'},
-    {x:13.81, y:56.72, w:50.52, h:3.60, url:'https://mze.gov.cz/public/app/eagriapp/lpisdata/'}
+  "02": [
+    { url: "https://cs.wikipedia.org/wiki/Okres_%C5%BD%C4%8F%C3%A1r_nad_S%C3%A1zavou#/media/Soubor:Okres_zdar_nad_sazavou.PNG", x: 70.30, y: 35.33, w: 6.94, h: 1.87 }
+  ],
+  "21": [
+    { url: "https://spu.gov.cz/pudni-sluzba/bpej", x: 13.87, y: 35.87, w: 31.20, h: 3.47 },
+    { url: "https://creativecommons.org/licenses/by/4.0/deed.cs", x: 46.04, y: 35.87, w: 8.40, h: 3.47 },
+    { url: "https://mze.gov.cz/public/app/eagriapp/lpisdata/", x: 13.87, y: 47.93, w: 41.54, h: 3.47 },
+    { url: "https://geoportal.cuzk.cz", x: 13.87, y: 64.47, w: 21.45, h: 3.47 },
+    { url: "https://creativecommons.org/licenses/by/4.0/deed.cs", x: 36.30, y: 64.47, w: 8.40, h: 3.47 },
+    { url: "https://cs.wikipedia.org/wiki/Okres_%C5%BD%C4%8F%C3%A1r_nad_S%C3%A1zavou#/media/Soubor:Okres_zdar_nad_sazavou.PNG", x: 13.87, y: 76.53, w: 49.23, h: 3.47 },
+    { url: "https://creativecommons.org/licenses/by-sa/4.0/deed.cs", x: 64.08, y: 76.53, w: 11.55, h: 3.47 }
   ]
 };
