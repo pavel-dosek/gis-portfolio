4fc5377d9b02011c895732d5d44a9f209aae54b7
pavel-dosek <pavel.dosek@gmail.com>
2026-09-11 10:58:29 +0200

    Add files via upload

--- davka-2/rekultivace/links.js
+++ davka-2/rekultivace/links.js
@@ -1,1 +1,8 @@
-window.__reportLinks=window.__reportLinks||{};window.__reportLinks['davka-2/rekultivace']={"05": [{"x": 47.76, "y": 18.62, "w": 34.86, "h": 1.6, "url": "https://cs.wikipedia.org/wiki/Lom_%C4%8CSA"}, {"x": 11.9, "y": 13.16, "w": 49.57, "h": 1.72, "url": "http://geoportal.cuzk.cz/WMS_ORTOFOTO_PUB/WMService.aspx"}]};
+window.__reportLinks = window.__reportLinks || {};
+window.__reportLinks['davka-2/rekultivace'] = {
+  "05": [
+    { url: "http://geoportal.cuzk.cz/WMS_ORTOFOTO_PUB/WMService.aspx", x: 11.91, y: 13.64, w: 57.44, h: 1.07 },
+    { url: "https://creativecommons.org/licenses/by/4.0/deed.cs", x: 62.88, y: 13.38, w: 9.37, h: 1.33 },
+    { url: "https://cs.wikipedia.org/wiki/Lom_%C4%8CSA", x: 47.76, y: 18.94, w: 34.76, h: 1.11 }
+  ]
+};
