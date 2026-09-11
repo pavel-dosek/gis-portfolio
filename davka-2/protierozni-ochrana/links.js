a36352a7524be171f9e3f2497ab944a64b8b01a6
pavel-dosek <pavel.dosek@gmail.com>
2026-09-11 19:27:23 +0200

    Add files via upload

--- davka-2/protierozni-ochrana/links.js
+++ davka-2/protierozni-ochrana/links.js
@@ -1,15 +1,11 @@
 window.__reportLinks = window.__reportLinks || {};
-window.__reportLinks['davka-2/protierozni-ochrana'] = {
-  "02": [
-    { url: "https://cs.wikipedia.org/wiki/Okres_%C5%BD%C4%8F%C3%A1r_nad_S%C3%A1zavou#/media/Soubor:Okres_zdar_nad_sazavou.PNG", x: 70.30, y: 35.33, w: 6.94, h: 1.87 }
-  ],
-  "31": [
-    { url: "https://spu.gov.cz/pudni-sluzba/bpej", x: 13.8, y: 40.2, w: 37.76, h: 3.53 },
-    { url: "https://creativecommons.org/licenses/by/4.0/deed.cs", x: 52.34, y: 40.2, w: 11.44, h: 3.53 },
-    { url: "https://mze.gov.cz/public/app/eagriapp/lpisdata/", x: 13.8, y: 56.73, w: 50.51, h: 3.53 },
-    { url: "https://geoportal.cuzk.cz", x: 13.8, y: 73.2, w: 25.95, h: 3.6 },
-    { url: "https://creativecommons.org/licenses/by/4.0/deed.cs", x: 40.49, y: 73.2, w: 11.44, h: 3.6 },
-    { url: "https://cs.wikipedia.org/wiki/Okres_%C5%BD%C4%8F%C3%A1r_nad_S%C3%A1zavou#/media/Soubor:Okres_zdar_nad_sazavou.PNG", x: 13.84, y: 89.73, w: 58.61, h: 3.6 },
-    { url: "https://creativecommons.org/licenses/by-sa/4.0/deed.cs", x: 73.27, y: 89.73, w: 14.7, h: 3.6 }
-  ]
-};
+window.__reportLinks['davka-2/protierozni-ochrana'] = window.__reportLinks['davka-2/protierozni-ochrana'] || {};
+window.__reportLinks['davka-2/protierozni-ochrana']['31'] = [
+  {url:'https://spu.gov.cz/pudni-sluzba/bpej', x:13.86, y:35.83, w:31.16, h:3.33},
+  {url:'https://creativecommons.org/licenses/by/4.0/', x:46.07, y:35.83, w:8.39, h:3.33},
+  {url:'https://mze.gov.cz/public/app/eagriapp/lpisdata/', x:13.86, y:47.96, w:41.53, h:3.33},
+  {url:'https://geoportal.cuzk.cz', x:13.86, y:64.44, w:21.42, h:3.33},
+  {url:'https://creativecommons.org/licenses/by/4.0/', x:36.32, y:64.44, w:8.39, h:3.33},
+  {url:'https://cs.wikipedia.org/wiki/Soubor:Okres_zdar_nad_sazavou.PNG', x:13.86, y:76.57, w:49.19, h:3.33},
+  {url:'https://creativecommons.org/licenses/by-sa/4.0/', x:64.10, y:76.57, w:11.52, h:3.33}
+];
