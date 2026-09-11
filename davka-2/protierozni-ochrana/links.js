8f990a522c1818855028fe52080280d5e2114c3d
pavel-dosek <pavel.dosek@gmail.com>
2026-09-11 09:33:25 +0200

    Add files via upload

--- davka-2/protierozni-ochrana/links.js
+++ davka-2/protierozni-ochrana/links.js
@@ -3,13 +3,13 @@ window.__reportLinks['davka-2/protierozni-ochrana'] = {
   "02": [
     { url: "https://cs.wikipedia.org/wiki/Okres_%C5%BD%C4%8F%C3%A1r_nad_S%C3%A1zavou#/media/Soubor:Okres_zdar_nad_sazavou.PNG", x: 70.30, y: 35.33, w: 6.94, h: 1.87 }
   ],
-  "21": [
-    { url: "https://spu.gov.cz/pudni-sluzba/bpej", x: 13.87, y: 35.87, w: 31.20, h: 3.47 },
-    { url: "https://creativecommons.org/licenses/by/4.0/deed.cs", x: 46.04, y: 35.87, w: 8.40, h: 3.47 },
-    { url: "https://mze.gov.cz/public/app/eagriapp/lpisdata/", x: 13.87, y: 47.93, w: 41.54, h: 3.47 },
-    { url: "https://geoportal.cuzk.cz", x: 13.87, y: 64.47, w: 21.45, h: 3.47 },
-    { url: "https://creativecommons.org/licenses/by/4.0/deed.cs", x: 36.30, y: 64.47, w: 8.40, h: 3.47 },
-    { url: "https://cs.wikipedia.org/wiki/Okres_%C5%BD%C4%8F%C3%A1r_nad_S%C3%A1zavou#/media/Soubor:Okres_zdar_nad_sazavou.PNG", x: 13.87, y: 76.53, w: 49.23, h: 3.47 },
-    { url: "https://creativecommons.org/licenses/by-sa/4.0/deed.cs", x: 64.08, y: 76.53, w: 11.55, h: 3.47 }
+  "31": [
+    { url: "https://spu.gov.cz/pudni-sluzba/bpej", x: 13.8, y: 40.2, w: 37.76, h: 3.53 },
+    { url: "https://creativecommons.org/licenses/by/4.0/deed.cs", x: 52.34, y: 40.2, w: 11.44, h: 3.53 },
+    { url: "https://mze.gov.cz/public/app/eagriapp/lpisdata/", x: 13.8, y: 56.73, w: 50.51, h: 3.53 },
+    { url: "https://geoportal.cuzk.cz", x: 13.8, y: 73.2, w: 25.95, h: 3.6 },
+    { url: "https://creativecommons.org/licenses/by/4.0/deed.cs", x: 40.49, y: 73.2, w: 11.44, h: 3.6 },
+    { url: "https://cs.wikipedia.org/wiki/Okres_%C5%BD%C4%8F%C3%A1r_nad_S%C3%A1zavou#/media/Soubor:Okres_zdar_nad_sazavou.PNG", x: 13.84, y: 89.73, w: 58.61, h: 3.6 },
+    { url: "https://creativecommons.org/licenses/by-sa/4.0/deed.cs", x: 73.27, y: 89.73, w: 14.7, h: 3.6 }
   ]
 };
